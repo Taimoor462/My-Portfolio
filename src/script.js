1192fe00b6beefb1b0063a924903a1978809ccb5
Taimoor Shah <tamoorshah462@gmail.com>
2024-08-07 19:58:43 +0500

    updated CV, skills and experience

--- src/script.js
+++ src/script.js
@@ -19,3 +19,22 @@ $(document).ready(function () {
     }
   });
 });
+
+
+function calculateExperience(startMonth, startYear) {
+  const startDate = new Date(startYear, startMonth - 1);
+  const currentDate = new Date();
+
+  let years = currentDate.getFullYear() - startDate.getFullYear();
+  let months = currentDate.getMonth() - startDate.getMonth();
+
+  if (months < 0) {
+    years--;
+    months += 12;
+  }
+
+  return `${years} years and ${months} months`;
+}
+
+const experience = calculateExperience(3, 2022);
+document.getElementById('experience').innerText = `${experience}`;
